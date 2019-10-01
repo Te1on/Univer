4f703d9e093bad7b511ab39ea8efa4ce587b8796
Te1on <54699412+Te1on@users.noreply.github.com>
2019-10-02 00:16:58 +0900

    Update example2-3.py

--- hw2/example2-3.py
+++ hw2/example2-3.py
@@ -1,16 +1,11 @@
-# Максимальное количество слов: 100.
-# Максимальная длина слова: [1, 12].
-# Все слова на вход подаются в нижнем регистре.
-
-
 def count_unique_codes(words):
     morse_signs = [
         ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
         "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
         "..-", "...-", ".--", "-..-", "-.--", "--.."
     ]
-    import string
-    morse_base = dict(zip(string.ascii_lowercase, morse_signs))
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    morse_base = dict(zip(alphabet,  morse_signs))
     cipher_sentence = []
     for word in words:
         cipher_word = ''
