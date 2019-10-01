b332f38fc6f72381cc81957fd31a0beb32a12b0f
Te1on <54699412+Te1on@users.noreply.github.com>
2019-10-02 00:17:31 +0900

    Update example2-4.py

--- hw2/example2-4.py
+++ hw2/example2-4.py
@@ -1,9 +1,12 @@
 def get_most_frequent(words, k):
-    import collections
-    return [w for w, k in (collections.Counter(words).most_common(k))]
+    a = set(words)
+    b = [words.count(word) for word in a]
+    d = sorted(zip(a, b), key=lambda x: x[1], reverse=True)
+    d = [v for v, k in d]
+    return d[:k]
 
 
 if __name__ == "__main__":
     print(get_most_frequent([
         "hello", "world", "hello", "my", "dear", "world", "hello"
-    ], 3))
+    ], 4))
