033c8fbc2232b1b5f1389622830f7acc6e82b9b9
Te1on <54699412+Te1on@users.noreply.github.com>
2019-10-02 01:31:36 +0900

    Update example2-7.py

--- hw2/example2-7.py
+++ hw2/example2-7.py
@@ -1,9 +1,9 @@
 def get_three_sum(nums, target):
-    for i in range(len(nums)):
+    d = dict(enumerate(nums))
+    for i in range(len(nums) - 1):
         for j in range(i + 1, len(nums)):
-            for k in range(j + 1, len(nums)):
-                if nums[i] + nums[j] + nums[k] == target:
-                    return i, j, k
+            if target - (nums[i] + nums[j]) in d.values():
+                return list(d.keys())[list(d.values()).index(target - (nums[i] + nums[j]))], i, j
     return None
 
 
